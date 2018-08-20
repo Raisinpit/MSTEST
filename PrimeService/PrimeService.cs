36453bf38d29ea3e3c3f1c61041b1f96b0163258
raisinpit <karim.tawfik1@hotmail.com>
2018-08-20 11:04:35 +0200

    kek

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+         public bool IsPrime0(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+         public bool IsPrime1(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+         public bool IsPrime2(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+         public bool IsPrime3(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+         public bool IsPrime4(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+         public bool IsPrime5(int candidate)
+        {
+            if (candidate < 2)
+            {   
+                try { 
+                return false;
+                }
+                catch (InvalidCastException e)   
+                {  
+                    throw e;  
+                }  
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+        
+    }
+}
